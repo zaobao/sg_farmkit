939527ca4a884d017452fe810fbf54f5580ebd45
zaobao <1205499934@qq.com>
2015-01-01 19:24:05 +0800

    Update sg_farmkit.js
    伐木按钮由原来左边放到右边，主题贴增加伐木按钮

--- sg_farmkit.js
+++ sg_farmkit.js
@@ -4,7 +4,7 @@
 // @description SG伐木助手
 // @include     http://bbs.sgamer.com/thread-*.html
 // @include     http://bbs.sgamer.com/*mod=viewthread*
-// @version     1
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 
@@ -28,12 +28,18 @@ for (var i = 0, isReply = 0; i < postNodes.length; i++) {
 					var span = document.createElement("span");
 					span.className = "pipe";
 					span.innerHTML = "|";
+					span.style.cssFloat = "right";
+					span.style.lineHeight = "16px";
+					span.style.margin = "0px 2px";
 					divs[j].appendChild(span);
 					divs[j].appendChild(document.createTextNode("\n"));
 					var farmArchor = document.createElement("a");
 					farmArchor.innerHTML = "复制伐木";
 					farmArchor.style.color = "green";
 					farmArchor.style.cursor = "pointer";
+					farmArchor.style.cssFloat = "right";
+					farmArchor.style.lineHeight = "16px";
+					farmArchor.style.padding = "0px 6px";
 					divs[j].appendChild(farmArchor);
 					divs[j].appendChild(document.createTextNode("\n"));
 					farmArchor.divPElement = divs[j];
@@ -54,6 +60,43 @@ for (var i = 0, isReply = 0; i < postNodes.length; i++) {
 				}
 			}
 		} else {
+			var divs = postNode.getElementsByTagName("div");
+			for (var j = 0; j < divs.length; j++) {
+				if (divs[j].className == "authi" && divs[j].parentNode.className == "pti") {
+					var span = document.createElement("span");
+					span.className = "pipe";
+					span.innerHTML = "|";
+					span.style.cssFloat = "right";
+					span.style.lineHeight = "16px";
+					span.style.margin = "0px 2px";
+					divs[j].appendChild(span);
+					divs[j].appendChild(document.createTextNode("\n"));
+					var farmArchor = document.createElement("a");
+					farmArchor.innerHTML = "快速伐木";
+					farmArchor.style.color = "green";
+					farmArchor.style.cursor = "pointer";
+					farmArchor.style.cssFloat = "right";
+					farmArchor.style.lineHeight = "16px";
+					farmArchor.style.padding = "0px 6px";
+					divs[j].appendChild(farmArchor);
+					divs[j].appendChild(document.createTextNode("\n"));
+					farmArchor.divPElement = divs[j];
+					farmArchor.onclick = function () {
+						var postText = "伐木伐木";
+						var tds = this.divPElement.parentNode.parentNode.parentNode.getElementsByTagName("td");
+						for (var k = 0; k < tds.length; k++) {
+							if (tds[k].getAttribute("id").match("postmessage_")) {
+								postText = tds[k].innerText || tds[k].textContent || tds[k].text;
+								if (postText[0] == "\n") {
+									postText = postText.slice(1);
+								}
+								break;
+							}
+						}
+						fastfarm(postText);
+					}
+				}
+			}
 			isReply = 1;
 		}
 	}
