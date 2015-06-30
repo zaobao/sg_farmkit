fcf5d73dbca510f6854736ea5e0fc520080ae704
zaobao <1205499934@qq.com>
2015-06-30 23:52:20 +0800

    version 2.1
    1、添加点评功能
    2、整理代码

--- sg_farmkit.js
+++ sg_farmkit.js
@@ -4,7 +4,7 @@
 // @description SG伐木助手
 // @include     http://bbs.sgamer.com/thread-*.html
 // @include     http://bbs.sgamer.com/*mod=viewthread*
-// @version     2.0.3
+// @version     2.1.0
 // @grant       none
 // ==/UserScript==
 
@@ -44,15 +44,79 @@ var rrr = {
 
 var fastFormNames = ["fastpostform", "vfastpostform"];
 
-function isParent(obj, parentObj){
-	while (obj != undefined && obj != null && obj.tagName.toUpperCase() != 'BODY') {
-		if (obj == parentObj){
-			return true;
+function createCommentButtonByReplyButton(fastre)
+{
+	var a = document.createElement("a");
+	a.className = "cmmnt";
+	a.onclick = function () {
+		showWindow('comment', this.href, 'get', 1);
+		setTimeout(function () {
+			var commentform = document.getElementById("commentform");
+			var action = commentform.action;
+			action = action.replace(/tid=[0-9]+/, fastre.href.match(/tid=[0-9]+/));
+			action = action.replace(/pid=[0-9]+/, fastre.href.match(/repquote=[0-9]+/)[0].replace("repquote","pid"));
+			commentform.action = action;
+		}, 500);
+	};
+	a.href = "forum.php?mod=misc&action=comment&tid=12360082&pid=30553315&extra=page%3D1&page=1";
+	a.appendChild(document.createTextNode("点评"));
+	return a;
+}
+
+function createFastReplyBox() {
+	var box = document.createElement("div");
+	box.style.cssText = "width: 150px; display: none";
+	box.appendChild(createFastReplyItem("油菜花"));
+	box.appendChild(createFastReplyItem("+1"));
+	box.appendChild(createFastReplyItem("就是这样"));
+	box.appendChild(createFastReplyItem("道理是这么个道理"));
+	box.appendChild(createFastReplyItem("卜是卜可以"));
+	box.appendChild(createFastReplyItem("妮说是就是"));
+	box.appendChild(createFastReplyItem("抽根烟，压压惊"));
+	box.appendChild(createFastReplyItem("关我pis"));
+	box.appendChild(createFastReplyItem("这个投票我就不参与了"));
+	box.appendChild(createFastReplyItem("我选择死亡"));
+	box.appendChild(createFastReplyItem("半天说不出一句话"));
+	box.appendChild(createFastReplyItem("XNMBYY"));
+	box.appendChild(createFastReplyItem("脏是妮脏"));
+	box.appendChild(createFastReplyItem("妮为什么这么毒"));
+	box.appendChild(createFastReplyItem("这件事窝已经报警"));
+	box.appendChild(createFastReplyItem("建议永丰"));
+	box.appendChild(createFastReplyItem("ZZWDJS！！！"));
+	box.appendChild(createFastReplyItem("什么节奏"));
+	box.onmouseout = function (e) {
+		if (isParent(e.relatedTarget, this.parentNode.parentNode)) {
+			return false;
 		}
-		obj = obj.parentNode;
+		this.style.display = "none";
 	}
-	return false;
-} 
+	return box;
+}
+
+function createFastReplyItem(str) {
+	var item = document.createElement("div");
+	item.appendChild(document.createTextNode(str));
+	item.style.cursor = "pointer";
+	item.style.color = "gray";
+	item.onclick = function () {
+		this.parentNode.style.display = "none";
+		replyFarm(this.parentNode.parentNode.fastre.href, this.innerHTML);
+	}
+	item.onmouseover = function () {item.style.color = "green"}
+	item.onmouseout = function () {item.style.color = "gray"}
+	return item;
+}
+
+// 回复主题
+function fastfarm(replyStr) {
+	if (getCookie("SG_farmkit_ifPostTimeLimit")) {
+		onNeedMoreTime();
+		return;
+	}
+	document.getElementById("fastpostmessage").value = precensore(recoverText(replyStr));
+	setTimeLimit();
+	document.getElementById("fastpostform").submit();
+}
 
 function getCookie(c_name) {
 	if (document.cookie.length > 0) {
@@ -69,86 +133,15 @@ function getCookie(c_name) {
 	return "";
 }
 
-function setCookie (c_name, value, expiresecs) {
-	var exdate = new Date();
-	exdate.setSeconds(exdate.getSeconds() + expiresecs);
-	document.cookie = c_name + "=" + escape(value) + ((expiresecs == null) ? "" : ";expires=" + exdate.toGMTString());
-}
-
-function setTimeLimit() {
-	setCookie("SG_farmkit_ifPostTimeLimit", "1", 16);
-}
-
-function precensore (str) {
-	if (str) {
-		for (var ch in pcrr) {
-			str = str.replace(new RegExp(ch, "ig"), pcrr[ch]);
+function isParent(obj, parentObj){
+	while (obj != undefined && obj != null && obj.tagName.toUpperCase() != 'BODY') {
+		if (obj == parentObj){
+			return true;
 		}
+		obj = obj.parentNode;
 	}
-	if (str.length < 10) {
-		str = str + "          ";
-	}
-	return str;
+	return false;
 }
-
-function recoverText (str) {
-	if (str) {
-		for (var ch in rrr) {
-			str = str.replace(new RegExp(ch, "ig"), rrr[ch]);
-		}
-	}
-	return str;
-}
-
-function precensoreFastForm(formName) {
-	var form = document.getElementById(formName);
-	if (form) {
-		form.onsubmit = function (e) {
-			this.message.value = parseurl(precensore(this.message.value));
-			setTimeLimit();
-			ajaxpost(formName, 'return_reply', 'return_reply', 'onerror');
-			return false;
-		}
-	}
-}
-
-function replaceFace(e) {
-	var nodes = e.childNodes;
-	for (var i = 0; i < nodes.length; i++) {
-		if (nodes[i].nodeName && nodes[i].nodeName.toLowerCase() == "img") {
-			var smilieid = nodes[i].getAttribute("smilieid");
-			if (smilieid >= 85 && smilieid <= 137) {
-				e.replaceChild(document.createTextNode("{:" + smilieid + ":}"), nodes[i]);
-			} else if (smilieid >= 343 && smilieid <= 419) {
-				e.replaceChild(document.createTextNode("{:6_" + smilieid + ":}"), nodes[i]);
-			} else if (smilieid == 169 || smilieid == 177 || smilieid == 170 || smilieid == 178) {
-				e.replaceChild(document.createTextNode("{:3_" + smilieid + ":}"), nodes[i]);
-			} else if (smilieid >= 171 && smilieid <= 176 || smilieid >= 179 && smilieid <= 181) {
-				e.replaceChild(document.createTextNode("{:7_" + smilieid + ":}"), nodes[i]);
-			} else if (smilieid >= 168 && smilieid <= 276) {
-				e.replaceChild(document.createTextNode("{:5_" + smilieid + ":}"), nodes[i]);
-			} else {
-				e.replaceChild(document.createTextNode("[img=" + nodes[i].width + "," + nodes[i].height + "]" + nodes[i].src + "[/img]"), nodes[i]);
-			}
-		}
-	}
-}
-
-(function () { 
-	document.getElementById("scrolltop").getElementsByTagName("a")[0].onclick = function () {
-		showWindow('reply', this.href);
-		setTimeout(function () {
-			precensoreFastForm("postform");
-		}, 500);
-		return false;
-	}
-})();
-
-(function () { 
-	for (var i = 0; i < fastFormNames.length; i++) {
-		precensoreFastForm(fastFormNames[i]);
-	}
-})();
 
 function onNeedMoreTime() {
 	if (document.getElementById('ntcwin')) {
@@ -188,26 +181,122 @@ function onNeedMoreTime() {
 	}, 2500);
 }
 
-function fastfarm(replyStr) {
+function precensore (str) {
+	if (str) {
+		for (var ch in pcrr) {
+			str = str.replace(new RegExp(ch, "ig"), pcrr[ch]);
+		}
+	}
+	if (str.length < 10) {
+		str = str + "          ";
+	}
+	return str;
+}
+
+function precensoreFastForm(formName) {
+	var form = document.getElementById(formName);
+	if (form) {
+		form.onsubmit = function (e) {
+			this.message.value = parseurl(precensore(this.message.value));
+			setTimeLimit();
+			ajaxpost(formName, 'return_reply', 'return_reply', 'onerror');
+			return false;
+		}
+	}
+}
+
+function recoverText (str) {
+	if (str) {
+		for (var ch in rrr) {
+			str = str.replace(new RegExp(ch, "ig"), rrr[ch]);
+		}
+	}
+	return str;
+}
+
+function replaceFace(e) {
+	var nodes = e.childNodes;
+	for (var i = 0; i < nodes.length; i++) {
+		if (nodes[i].nodeName && nodes[i].nodeName.toLowerCase() == "img") {
+			var smilieid = nodes[i].getAttribute("smilieid");
+			if (smilieid >= 85 && smilieid <= 137) {
+				e.replaceChild(document.createTextNode("{:" + smilieid + ":}"), nodes[i]);
+			} else if (smilieid >= 343 && smilieid <= 419) {
+				e.replaceChild(document.createTextNode("{:6_" + smilieid + ":}"), nodes[i]);
+			} else if (smilieid == 169 || smilieid == 177 || smilieid == 170 || smilieid == 178) {
+				e.replaceChild(document.createTextNode("{:3_" + smilieid + ":}"), nodes[i]);
+			} else if (smilieid >= 171 && smilieid <= 176 || smilieid >= 179 && smilieid <= 181) {
+				e.replaceChild(document.createTextNode("{:7_" + smilieid + ":}"), nodes[i]);
+			} else if (smilieid >= 168 && smilieid <= 276) {
+				e.replaceChild(document.createTextNode("{:5_" + smilieid + ":}"), nodes[i]);
+			} else {
+				e.replaceChild(document.createTextNode("[img=" + nodes[i].width + "," + nodes[i].height + "]" + nodes[i].src + "[/img]"), nodes[i]);
+			}
+		}
+	}
+}
+
+function replyFarm(url, str) {
 	if (getCookie("SG_farmkit_ifPostTimeLimit")) {
 		onNeedMoreTime();
 		return;
 	}
-	document.getElementById("fastpostmessage").value = precensore(recoverText(replyStr));
-	setTimeLimit();
-	document.getElementById("fastpostform").submit();
+	var reply_box = document.createElement("div");
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", url + "&infloat=yes&handlekey=reply&inajax=1&ajaxtarget=fwin_content_reply",true);
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+			reply_box.innerHTML = xmlhttp.responseXML.documentElement.childNodes[0].data;
+			document.getElementById("postform").message.value = precensore(recoverText(str));
+			setTimeLimit();
+			document.getElementById("postform").submit();
+		}
+	}
+	xmlhttp.send();
+	document.body.appendChild(reply_box);
 }
 
+function setCookie (c_name, value, expiresecs) {
+	var exdate = new Date();
+	exdate.setSeconds(exdate.getSeconds() + expiresecs);
+	document.cookie = c_name + "=" + escape(value) + ((expiresecs == null) ? "" : ";expires=" + exdate.toGMTString());
+}
+
+function setTimeLimit() {
+	setCookie("SG_farmkit_ifPostTimeLimit", "1", 16);
+}
+
+// 回复栏和快速回复栏
+(function () { 
+	for (var i = 0; i < fastFormNames.length; i++) {
+		precensoreFastForm(fastFormNames[i]);
+	}
+})();
+
+// 侧边栏弹出回复栏
+(function () { 
+	document.getElementById("scrolltop").getElementsByTagName("a")[0].onclick = function () {
+		showWindow('reply', this.href);
+		setTimeout(function () {
+			precensoreFastForm("postform");
+		}, 500);
+		return false;
+	}
+})();
+
+// 页面标题
 (function () {
 	var tt = document.title;
 	document.title = recoverText(tt);
 })();
 
+// 主题标题
 (function () {
 	var ih = document.getElementById("thread_subject").innerHTML;
 	document.getElementById("thread_subject").innerHTML = recoverText(ih);
 })();
 
+// 帖子顶栏、内容和底部
 (function () {
 	var postlist = document.getElementById("postlist");
 	if (postlist == null) {
@@ -222,17 +311,26 @@ function fastfarm(replyStr) {
 			postNode.getAttribute("id").match("post_")) {
 			var tds = postNode.getElementsByTagName("td");
 			for (var k = 0; k < tds.length; k++) {
+				// 内容
 				if (tds[k].id && tds[k].id.match("postmessage_")) {
 					tds[k].innerHTML = recoverText(tds[k].innerHTML);
 				}
 			}
 			var fastre = null;
 			var as = postNode.getElementsByTagName("a");
-			for (var k = 0; k < as.length; k++) {
+			var asLength = as.length;
+			for (var k = 0; k < asLength; k++) {
 				if (as[k].className == "fastre") {
 					fastre = as[k];
+					// 底部
+					var firstNode = fastre.parentNode.getElementsByTagName("a")[0];
+					if (firstNode.className != "cmmnt")
+					{
+						fastre.parentNode.insertBefore(createCommentButtonByReplyButton(fastre), fastre);
+					}
 				}
 			}
+			// 顶部
 			if (isReply == 1) {
 				var divs = postNode.getElementsByTagName("div");
 				for (var j = 0; j < divs.length; j++) {
@@ -398,70 +496,6 @@ function fastfarm(replyStr) {
 		}
 	}
 })();
-
-function createFastReplyBox() {
-	var box = document.createElement("div");
-	box.style.cssText = "width: 150px; display: none";
-	box.appendChild(createFastReplyItem("油菜花"));
-	box.appendChild(createFastReplyItem("+1"));
-	box.appendChild(createFastReplyItem("就是这样"));
-	box.appendChild(createFastReplyItem("道理是这么个道理"));
-	box.appendChild(createFastReplyItem("卜是卜可以"));
-	box.appendChild(createFastReplyItem("妮说是就是"));
-	box.appendChild(createFastReplyItem("抽根烟，压压惊"));
-	box.appendChild(createFastReplyItem("关我pis"));
-	box.appendChild(createFastReplyItem("这个投票我就不参与了"));
-	box.appendChild(createFastReplyItem("我选择死亡"));
-	box.appendChild(createFastReplyItem("半天说不出一句话"));
-	box.appendChild(createFastReplyItem("XNMBYY"));
-	box.appendChild(createFastReplyItem("脏是妮脏"));
-	box.appendChild(createFastReplyItem("妮为什么这么毒"));
-	box.appendChild(createFastReplyItem("这件事窝已经报警"));
-	box.appendChild(createFastReplyItem("建议永丰"));
-	box.appendChild(createFastReplyItem("ZZWDJS！！！"));
-	box.appendChild(createFastReplyItem("什么节奏"));
-	box.onmouseout = function (e) {
-		if (isParent(e.relatedTarget, this.parentNode.parentNode)) {
-			return false;
-		}
-		this.style.display = "none";
-	}
-	return box;
-}
-
-function createFastReplyItem(str) {
-	var item = document.createElement("div");
-	item.appendChild(document.createTextNode(str));
-	item.style.cursor = "pointer";
-	item.style.color = "gray";
-	item.onclick = function () {
-		this.parentNode.style.display = "none";
-		replyFarm(this.parentNode.parentNode.fastre.href, this.innerHTML);
-	}
-	item.onmouseover = function () {item.style.color = "green"}
-	item.onmouseout = function () {item.style.color = "gray"}
-	return item;
-}
-
-function replyFarm(url, str) {
-	if (getCookie("SG_farmkit_ifPostTimeLimit")) {
-		onNeedMoreTime();
-		return;
-	}
-	var reply_box = document.createElement("div");
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("GET", url + "&infloat=yes&handlekey=reply&inajax=1&ajaxtarget=fwin_content_reply",true);
-	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-			reply_box.innerHTML = xmlhttp.responseXML.documentElement.childNodes[0].data;
-			document.getElementById("postform").message.value = precensore(recoverText(str));
-			setTimeLimit();
-			document.getElementById("postform").submit();
-		}
-	}
-	xmlhttp.send();
-	document.body.appendChild(reply_box);
-}
 
 if (devmode) {
 	alert("耗时：" + (new Date().getTime() - timestamp.getTime()) + "毫秒");
